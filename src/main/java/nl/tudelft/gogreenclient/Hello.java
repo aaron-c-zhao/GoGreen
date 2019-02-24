792b4a946a509972f91213e8ceb534caf1fa181e
aaronzhao <C.Zhao-1@student.tudelft.nl>
2019-02-24 22:24:49 +0100

    demo1

---
+++ src/main/java/nl/tudelft/gogreenclient/Hello.java
@@ -0,0 +1,32 @@
+package nl.tudelft.gogreenclient;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Hello {
+	
+	private String name;
+	private LocalDateTime time;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LocalDateTime getTime() {
+		return time;
+	}
+	public void setTime(LocalDateTime time) {
+		this.time = time;
+	}
+	
+	@Override
+	public String toString() {
+		return "Hello " + name +
+				"\nIt's" + time.toString() + " now";
+	}
+	
+}
