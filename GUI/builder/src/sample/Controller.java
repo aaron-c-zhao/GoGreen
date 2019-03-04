777c977b0a662334345fa8a4fda0880b46c26138
aimarinov <marinov.atanas99@gmail.com>
2019-03-04 10:48:20 +0100

    adding gui source code

---
+++ GUI/builder/src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Controller {
+    public void switchToVegetarianMeal(){
+        Main.window.setScene(Main.vegetarian_meal);
+    }
+    public void switchToMenu(){
+        Main.window.setScene(Main.menu);
+    }
+}
---
+++ GUI/builder/src/sample/Profile_Controller.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
+import java.awt.*;
+
+public class Profile_Controller {
+    @FXML
+    public TextField textField;
+
+    final PasswordField pb = new PasswordField();
+
+   /* textField.textProperty().addListener(new ChangeListener<String>() {
+        @Override
+        public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+            if (!newValue.matches("\\d{0,7}([\\.]\\d{0,4})?")) {
+                vehiclePrice_TextField.setText(oldValue);
+            }
+        }
+    });*/
+   /* public void initialize(){
+        textField.addTextListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!newValue.matches("\\d{0,7}([\\.]\\d{0,4})?")) {
+                    vehiclePrice_TextField.setText(oldValue);
+                }
+            }
+        });
+    }*/
+}
