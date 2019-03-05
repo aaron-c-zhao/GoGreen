652f067d7dd3d980025730fe43a9a0b79d3b0826
Chengrui Zhao <c.zhao-1@student.tudelft.nl>
2019-03-05 20:51:35 +0000

    Update GogreenApplicationTest.java

--- gogreen-webserver/src/test/java/gogreenserver/GogreenApplicationTest.java
+++ gogreen-webserver/src/test/java/gogreenserver/GogreenApplicationTest.java
@@ -3,16 +3,16 @@ package gogreenserver;
 
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
 public class GogreenApplicationTest {
 
 // 	@Autowired
