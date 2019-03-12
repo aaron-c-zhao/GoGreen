ff08509a73e1f87acd4668fb640167528842a711
Nik Kapitonenko <m.kapitonenko@student.tudelft.nl>
2019-03-12 22:26:28 +0100

    Uncommented basic greeting (echo), moved and renamed the demo 1 controller to a more fitting name and proper location

---
+++ gogreen-webserver/src/main/java/gogreenserver/controllers/MiscController.java
@@ -0,0 +1,48 @@
+package gogreenserver.controllers;
+
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * MVC controller for miscellaneous end points.
+ *
+ */
+@RestController
+@RequestMapping("/api")
+public class MiscController {
+
+    /**
+     * A simple echo end point.
+     * @param name The value to echo back.
+     * @return A {@link Greeting} class, converted to JSON, containing the name and date.
+     */
+    @GetMapping("/greeting/{name}")
+    public Greeting greeting(@PathVariable(value = "name") String name) {
+        return new Greeting(name);
+    }
+    
+    public class Greeting {
+
+        private final String name;
+        private final LocalDateTime time;
+
+        public Greeting(String name) {
+            this.name = name;
+            this.time = LocalDateTime.now();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public LocalDateTime getTime() {
+            return time;
+        }
+    }
+}
