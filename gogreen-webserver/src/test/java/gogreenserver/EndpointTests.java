ba0060e4655566cb3567d007897bbac1c98f5dcf
Nik Kapitonenko <m.kapitonenko@student.tudelft.nl>
2019-03-13 19:02:54 +0100

    Added a mock db.

--- gogreen-webserver/src/test/java/gogreenserver/EndpointTests.java
+++ gogreen-webserver/src/test/java/gogreenserver/EndpointTests.java
@@ -1,26 +1,50 @@
 package gogreenserver;
 
+import gogreenserver.controllers.MiscController;
+import gogreenserver.entity.User;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import gogreenserver.controllers.MiscController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RunWith(SpringRunner.class)
 @WebMvcTest(MiscController.class)
+@DataJpaTest
 public class EndpointTests {
     @Autowired
     private MockMvc mockMvc;
+    
+    @Autowired
+    private TestEntityManager eManager;
+    
+    User mockUser1 = new User();
+    //User mockUser2 = new User();
+    
+    @Before
+    public void setupMockDB()
+    {
+        mockUser1.setFirstName("First");
+        mockUser1.setLastName("Last");
+        //mockUser2.setFirstName("First");
+        //mockUser2.setLastName("Last");
+        eManager.persist(mockUser1);
+    }
 
     @Test
     public void echo() throws Exception {
@@ -29,18 +53,18 @@ public class EndpointTests {
 
         RequestBuilder request = MockMvcRequestBuilders.get("/api/greeting/" + name)
                 .accept(MediaType.APPLICATION_JSON);
-
         mockMvc.perform(request).andExpect(status().is(200))
                 .andExpect(content().json("{\"name\":\"" + name + "\"}")).andReturn();
     }
 
     @Test
     public void checkUsers() throws Exception {
-
-        RequestBuilder listReq = MockMvcRequestBuilders.get("/api/users/")
+        //FIXME why does this not work?
+        RequestBuilder listReq = MockMvcRequestBuilders.get("/api/users")
                 .accept(MediaType.APPLICATION_JSON);
-
-        var res = mockMvc.perform(listReq).andExpect(status().is(200)).andReturn();
+        MvcResult res = mockMvc.perform(listReq).andExpect(status().is(200)).andReturn();
+        
+        
         JsonNode list = (new ObjectMapper()).readTree(res.getResponse().getContentAsString());
         for (JsonNode user : list) {
             RequestBuilder userReq = MockMvcRequestBuilders.get("/api/users/" + user.asText())
