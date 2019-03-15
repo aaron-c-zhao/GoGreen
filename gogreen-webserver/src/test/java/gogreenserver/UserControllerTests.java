30282450236d6b184479a7ec126fe913da970b8d
Nik Kapitonenko <m.kapitonenko@student.tudelft.nl>
2019-03-15 15:46:03 +0100

    Made that test work somehow
    But currently, this test is useless, I need to quickly figure out how to
    properly do integration testing...

--- gogreen-webserver/src/test/java/gogreenserver/UserControllerTests.java
+++ gogreen-webserver/src/test/java/gogreenserver/UserControllerTests.java
@@ -6,12 +6,18 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gogreenserver.controllers.UserController;
+import gogreenserver.repositories.UserRepository;
+import gogreenserver.services.UserService;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -20,9 +26,17 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
+
 public class UserControllerTests {
+
+    // for debugging purposes
+    private static final Logger LOGGER = LogManager.getLogger("UserController Tester");
+
     @Autowired
     private MockMvc mockMvc;
+    
+    @MockBean
+    private UserService service;
 
     @Test
     public void checkUsers() throws Exception {
@@ -31,7 +45,11 @@ public class UserControllerTests {
         MvcResult res = mockMvc.perform(listReq).andExpect(status().is(200)).andReturn();
 
         JsonNode list = (new ObjectMapper()).readTree(res.getResponse().getContentAsString());
+        
+        LOGGER.error(list);
+        
         for (JsonNode user : list) {
+            LOGGER.debug(user);
             RequestBuilder userReq = MockMvcRequestBuilders.get("/api/users/" + user.asText())
                     .accept(MediaType.APPLICATION_JSON);
             mockMvc.perform(userReq).andExpect(status().is(200)).andReturn();
