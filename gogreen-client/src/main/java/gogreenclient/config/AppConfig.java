8000565383a387885c46da7435081e87b004f1e8
aaronzhao <C.Zhao-1@student.tudelft.nl>
2019-03-15 09:13:14 +0100

    HTTPS done

--- gogreen-client/src/main/java/gogreenclient/config/AppConfig.java
+++ gogreen-client/src/main/java/gogreenclient/config/AppConfig.java
@@ -7,6 +7,7 @@ import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.tomcat.util.net.SSLUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.*;
@@ -15,6 +16,7 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
+import java.io.FileInputStream;
 import java.security.KeyStore;
 
 
@@ -46,9 +48,10 @@ public class AppConfig {
      */
     @Bean
     public RestTemplate restTemplate() throws Exception {
+        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        ks.load(new FileInputStream(keyStore.getFile()), keyStorePassword.toCharArray());
         SSLContext sslContext = new SSLContextBuilder()
-            //TODO
-            .loadKeyMaterial(keyStore.getURL(), keyStorePassword.toCharArray(), null)
+            .loadKeyMaterial(ks, keyStorePassword.toCharArray())
             .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray())
             .build();
         SSLConnectionSocketFactory socketFactory =
