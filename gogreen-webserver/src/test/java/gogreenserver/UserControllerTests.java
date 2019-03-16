c842e5200fa003fd44c7e7259feaf3c905252408
Nik Kapitonenko <m.kapitonenko@student.tudelft.nl>
2019-03-16 16:03:57 +0100

    Fixed the tests, again
    Apparently, if you access /api/user*s*/anything here, the server will
    still send a 200 back

--- gogreen-webserver/src/test/java/gogreenserver/UserControllerTests.java
+++ gogreen-webserver/src/test/java/gogreenserver/UserControllerTests.java
@@ -1,5 +1,6 @@
 package gogreenserver;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -16,6 +17,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,29 +35,41 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.time.LocalDate;
 import java.util.Random;
 
+import javax.transaction.Transactional;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+
+//set up test mvc
 @AutoConfigureWebMvc
 @AutoConfigureMockMvc
+
+//set up test db
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase
+@AutoConfigureTestEntityManager
+@Transactional
 public class UserControllerTests {
 
     // for debugging purposes
     private static final Logger LOGGER = LogManager.getLogger("Tests");
+    private static User[] dummyUsers;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private UserController controller;
+    private TestEntityManager manager;
 
     /**
      * Setup mock db.
      */
     @Before
     public void setupH2() {
-        controller.addUser(createDummyUser("Alice"));
-        controller.addUser(createDummyUser("Bob"));
-        controller.addUser(createDummyUser("Charlie"));
+        dummyUsers = new User[3];
+        dummyUsers[0] = manager.persist(createDummyUser("alice"));
+        dummyUsers[1] = manager.persist(createDummyUser("bob"));
+        dummyUsers[2] = manager.persist(createDummyUser("charlie"));
         LOGGER.debug("Set up the 3 dummy users");
     }
 
@@ -71,13 +88,19 @@ public class UserControllerTests {
 
         JsonNode list = (new ObjectMapper()).readTree(res.getResponse().getContentAsString());
 
-        LOGGER.debug(list);
+        LOGGER.debug("Returned Json: " + list);
 
+        int usercount = 0;
         for (JsonNode user : list) {
-            LOGGER.debug(user);
-            RequestBuilder userReq = MockMvcRequestBuilders.get("/api/users/" + user.asText())
+            LOGGER.debug("User " + usercount + ": " + user);
+            RequestBuilder userReq = MockMvcRequestBuilders
+                    .get("/api/user/" + user.get("username").asText())
                     .accept(MediaType.APPLICATION_JSON);
-            mockMvc.perform(userReq).andExpect(status().is(200)).andReturn();
+            MvcResult ures = mockMvc.perform(userReq).andExpect(status().is(200)).andReturn();
+
+            usercount++;
         }
+        LOGGER.debug("User amount: " + usercount);
+        assertThat(usercount).isEqualTo(3);
     }
 }
