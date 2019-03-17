cd97ed53e6cad128598c034bf9847f90e44b51b3
Alin Prundeanu <A.Prundeanu@student.tudelft.nl>
2019-03-17 15:23:39 +0100

    Another try for testing

--- gogreen-client/src/test/java/gogreenclient/GoGreenApplicationTest.java
+++ gogreen-client/src/test/java/gogreenclient/GoGreenApplicationTest.java
@@ -5,8 +5,6 @@ import javafx.stage.Stage;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
-import static org.junit.Assert.*;
-
 public class GoGreenApplicationTest extends ApplicationTest {
 
     @Test
