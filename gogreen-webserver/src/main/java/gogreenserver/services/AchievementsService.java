7b8b5acace5b084a383993f7ed2adc38882c4e74
Rahul <R.C.Kalaria@student.tudelft.nl>
2019-03-21 20:01:26 +0100

    added services for "Achiements","AddSolarpanels" entity

---
+++ gogreen-webserver/src/main/java/gogreenserver/services/AchievementsService.java
@@ -0,0 +1,32 @@
+package gogreenserver.services;
+
+import gogreenserver.entity.Achievements;
+import gogreenserver.repositories.AchievementsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AchievementsService {
+
+    private AchievementsRepository achievementsRepo;
+
+    @Autowired
+    public AchievementsService(AchievementsRepository achievementsRepository){
+        this.achievementsRepo = achievementsRepository;
+    }
+
+    public List<Achievements> findAll(){
+        return achievementsRepo.findAll();
+    }
+
+    public Optional<Achievements> findById(String userName){
+       return achievementsRepo.findById(userName);
+    }
+
+    // Yet have to add method to find Friends Achievements.
+
+
+}
---
+++ gogreen-webserver/src/main/java/gogreenserver/services/AddSolarpanelsService.java
@@ -0,0 +1,34 @@
+package gogreenserver.services;
+
+import gogreenserver.entity.AddSolarpanels;
+import gogreenserver.repositories.AddSolarpanelsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AddSolarpanelsService {
+
+    private AddSolarpanelsRepository addSolarpanelsRepo;
+
+    @Autowired
+    public AddSolarpanelsService(AddSolarpanelsRepository addSolarpanelsRepo){
+        this.addSolarpanelsRepo = addSolarpanelsRepo;
+    }
+
+    public List<AddSolarpanels> findAll(){
+        addSolarpanelsRepo.findAll();
+    }
+
+    public AddSolarpanels createAddSolarpanels(AddSolarpanels addSolarpanels){
+        return addSolarpanelsRepo.save(addSolarpanels);
+    }
+
+    public Optional<AddSolarpanels> findById(String userName){
+        return addSolarpanelsRepo.findById(userName);
+    }
+
+
+}
