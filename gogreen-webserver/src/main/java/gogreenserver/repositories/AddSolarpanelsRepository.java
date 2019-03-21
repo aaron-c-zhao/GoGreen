f3c91bf9a367d6158a9ed57d15796e781cb11d5a
Rahul <R.C.Kalaria@student.tudelft.nl>
2019-03-21 19:43:19 +0100

    added the "AddSolarpanelsRepository"

---
+++ gogreen-webserver/src/main/java/gogreenserver/repositories/AddSolarpanelsRepository.java
@@ -0,0 +1,7 @@
+package gogreenserver.repositories;
+
+import gogreenserver.entity.AddSolarpanels;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddSolarpanelsRepository extends JpaRepository<AddSolarpanels, String> {
+}
