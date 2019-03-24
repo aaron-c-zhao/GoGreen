aa0e655ba25832cccaac3886db8bea85cf89e3d3
Rahul <R.C.Kalaria@student.tudelft.nl>
2019-03-24 09:36:35 +0100

    updated Achievemnets entity

--- gogreen-webserver/src/main/java/gogreenserver/controllers/AchievementsController.java
+++ gogreen-webserver/src/main/java/gogreenserver/controllers/AchievementsController.java
@@ -1,6 +1,5 @@
 package gogreenserver.controllers;
 
-import gogreenserver.entity.Achievements;
 import gogreenserver.services.AchievementsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
--- gogreen-webserver/src/main/java/gogreenserver/repositories/AchievementsRepository.java
+++ gogreen-webserver/src/main/java/gogreenserver/repositories/AchievementsRepository.java
@@ -1,6 +1,5 @@
 package gogreenserver.repositories;
 
-import gogreenserver.entity.Achievements;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AchievementsRepository extends JpaRepository<Achievements, String> {
