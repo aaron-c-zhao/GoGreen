2d9e37dd2ec5b95c821061f1eff375c85c39760d
Rahul <R.C.Kalaria@student.tudelft.nl>
2019-03-24 10:13:54 +0100

    Fixed checkstyle errors -.-''

--- gogreen-webserver/src/main/java/gogreenserver/entity/InsertHistory.java
+++ gogreen-webserver/src/main/java/gogreenserver/entity/InsertHistory.java
@@ -1,10 +1,10 @@
 package gogreenserver.entity;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Date;
 
 @Entity
 @Table(name = "insert_history")
