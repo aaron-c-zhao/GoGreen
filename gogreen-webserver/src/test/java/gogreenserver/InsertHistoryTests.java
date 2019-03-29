c26415a482aed2787bd561dbffb029f6c28fdb10
Nik Kapitonenko <m.kapitonenko@student.tudelft.nl>
2019-03-29 14:42:24 +0100

    Add final tests
    Also, use Spring's query-building mechanism instead of filtering
    insertHistory's ourself.

--- gogreen-webserver/src/test/java/gogreenserver/InsertHistoryTests.java
+++ gogreen-webserver/src/test/java/gogreenserver/InsertHistoryTests.java
@@ -4,9 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gogreenserver.entity.InsertHistory;
+import gogreenserver.entity.InsertHistoryCo2;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -30,6 +32,8 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDateTime;
 
 import javax.transaction.Transactional;
 
@@ -91,15 +95,123 @@ public class InsertHistoryTests {
                 .header("userName", dummy.getUserName());
         mockMvc.perform(req).andExpect(status().is(200));
 
-        //This is not how the real db works, but we don't have the real db, so...
+        manager.flush();
+        // This is not how the real db works, but we don't have the real db, so...
         InsertHistory found = manager.find(InsertHistory.class, 1L);
         assertThat(found).isNotNull();
 
         manager.clear();
     }
 
+    @WithMockUser
+    @Test
+    public void retrieveRecentHistory() throws Exception {
+
+        LOGGER.debug("=== retrieveRecentHistory() ===");
+
+        InsertHistoryCo2 first = createDummyInsertHistoryCo2("Theta");
+        InsertHistoryCo2 second = createDummyInsertHistoryCo2("Theta");
+        InsertHistoryCo2 third = createDummyInsertHistoryCo2("Theta");
+
+        first.setInsertDate(LocalDateTime.of(2019, 1, 1, 1, 1));
+        second.setInsertDate(LocalDateTime.of(2019, 1, 1, 2, 1));
+        third.setInsertDate(LocalDateTime.of(2019, 1, 1, 3, 1));
+
+        manager.persist(second);
+        manager.persist(third);
+        manager.persist(first);
+
+        manager.flush();
+
+        RequestBuilder req = MockMvcRequestBuilders.get("/api/insertHistory/Theta")
+                .accept(MediaType.APPLICATION_JSON);
+        JsonNode list = mapper.readTree(mockMvc.perform(req).andExpect(status().is(200)).andReturn()
+                .getResponse().getContentAsString());
+
+        JsonNode alpha = list.get(0);
+        JsonNode beta = list.get(1);
+        assertThat(list.get(2)).isNull();
+        LOGGER.debug("First response: " + alpha.get("insertDate").asText());
+        LOGGER.debug("Second response: " + beta.get("insertDate").asText());
+
+        assertThat(alpha.get("insertDate").asText()).isEqualTo("2019-01-01T03:01:00");
+        assertThat(beta.get("insertDate").asText()).isEqualTo("2019-01-01T02:01:00");
+        
+        manager.clear();
+    }
+
+    @WithMockUser
+    @Test
+    public void checkHistoryRange() throws Exception {
+
+        LOGGER.debug("=== checkHistoryRange() ===");
+
+        InsertHistoryCo2 first = createDummyInsertHistoryCo2("Theta");
+        InsertHistoryCo2 second = createDummyInsertHistoryCo2("Theta");
+        InsertHistoryCo2 third = createDummyInsertHistoryCo2("Theta");
+
+        first.setInsertDate(LocalDateTime.of(2019, 1, 1, 0, 0));
+        second.setInsertDate(LocalDateTime.of(2019, 1, 2, 0, 0));
+        third.setInsertDate(LocalDateTime.of(2019, 1, 4, 0, 0));
+
+        manager.persist(second);
+        manager.persist(third);
+        manager.persist(first);
+
+        manager.flush();
+
+        RequestBuilder req = MockMvcRequestBuilders.get("/api/insertHistory/days/Theta")
+                .accept(MediaType.APPLICATION_JSON);
+
+        String res = mockMvc.perform(req).andExpect(status().is(200)).andReturn().getResponse()
+                .getContentAsString();
+        LOGGER.debug(res);
+
+        assertThat(res).isEqualTo("3");
+        
+        manager.clear();
+    }
+
+    @WithMockUser
+    @Test
+    public void checkHistoryAmount() throws Exception {
+
+        LOGGER.debug("=== checkHistoryAmount() ===");
+
+        InsertHistoryCo2 first = createDummyInsertHistoryCo2("Theta");
+        InsertHistoryCo2 second = createDummyInsertHistoryCo2("Theta");
+        InsertHistoryCo2 third = createDummyInsertHistoryCo2("Theta");
+
+        first.setInsertDate(LocalDateTime.of(2019, 1, 1, 0, 0));
+        second.setInsertDate(LocalDateTime.of(2019, 1, 2, 0, 0));
+        third.setInsertDate(LocalDateTime.of(2019, 1, 4, 0, 0));
+
+        manager.persist(second);
+        manager.persist(third);
+        manager.persist(first);
+
+        manager.flush();
+
+        RequestBuilder req = MockMvcRequestBuilders.get("/api/insertHistory/amount/Theta")
+                .accept(MediaType.APPLICATION_JSON);
+
+        String res = mockMvc.perform(req).andExpect(status().is(200)).andReturn().getResponse()
+                .getContentAsString();
+        LOGGER.debug(res);
+
+        assertThat(res).isEqualTo("3");
+        
+        manager.clear();
+    }
+
     private InsertHistory createDummyInsertHistory(String name) {
         InsertHistory history = new InsertHistory();
+        history.setUserName(name);
+        return history;
+    }
+
+    private InsertHistoryCo2 createDummyInsertHistoryCo2(String name) {
+        InsertHistoryCo2 history = new InsertHistoryCo2();
         history.setUserName(name);
         return history;
     }
